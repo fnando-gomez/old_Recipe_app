f6f05e517cf2c0343863d56ed5f3ccea2735293b
fnando <fnando.gomez@gmail.com>
2020-01-27 04:36:43 -0600

    Going to rest, CSS not done yet

--- dist/main.js
+++ dist/main.js
@@ -1,20 +1,20 @@
 
-const render =  new Renderer()
+const render = new Renderer()
 
 //With "Enter"
-let input = $('#ing').keypress(function(event){
-    if (event.keyCode === 13){
+let input = $('#ing').keypress((event) => {
+    if (event.keyCode === 13) {
         fetchRecipe()
     }
 })
 
 
-const fetchRecipe = function(){
+const fetchRecipe = function () {
     let input = $('#ing').val()
     console.log(input)
-    $.get(`/recipes/${input}`, function(data){
-        render.renderRecipes(data)
-    })
+    $.get(`/recipes/${input}`, (data) => { render.renderRecipes(data) })
+     
+    
 }
 
 
